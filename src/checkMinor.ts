d7d09f286c06308e13e6dfbdfe9d8477eb394c69
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-06-03 23:57:32 +0900

    Update checkMinor.ts

--- src/checkMinor.ts
+++ src/checkMinor.ts
@@ -1,5 +1,5 @@
 export function minor(text: string|any): boolean {
-    if(!text) throw new Error('Korean: 확인할 텍스트를 입력해 주세요');
+    if(!text) throw new Error('Korcen: 확인할 텍스트를 입력해 주세요');
     if(typeof text !== 'string') throw new Error('Korean: String 타입만 입력 가능합니다');
     const newtext = text.toLowerCase()
     text = newtext.replace(/[^가-힣]/gi, '')
