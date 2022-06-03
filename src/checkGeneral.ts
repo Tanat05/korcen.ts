224ddea4e886b569e82ffe5e82fe85457c1ef953
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-06-03 23:55:12 +0900

    Update checkGeneral.ts

--- src/checkGeneral.ts
+++ src/checkGeneral.ts
@@ -1,5 +1,5 @@
 export function general(text: string|any): boolean {
-    if(!text) throw new Error('Korean: 확인할 텍스트를 입력해 주세요');
+    if(!text) throw new Error('Korcen: 확인할 텍스트를 입력해 주세요');
     if(typeof text !== 'string') throw new Error('Korean: String 타입만 입력 가능합니다');
     const newtext = text.toLowerCase()
     text = newtext.replace(/ /gi, '')
@@ -44,7 +44,7 @@ export function general(text: string|any): boolean {
     text = text.replace(/qkf/gi, '발')
     text = text.replace(/qk/gi, '바')
     text = text.replace(/[^ㄱ-힣]/gi, '')
-    let fuck = ["시ㅂ", "시ㅏㄹ", "씨ㅂ", "씨ㅏㄹ", "ㅣ발", "ㅆ발", "ㅅ발", "ㅅㅂ", "ㅆㅂ", "ㅆ바", "ㅅ바", "시ㅂㅏ", "ㅅㅂㅏ", "시ㅏㄹ", "씨ㅏㄹ", "ㅅ불", "ㅆ불","ㅅ쁠", "ㅆ뿔"]
+    let fuck = ["시ㅂ", "시ㅏㄹ", "씨ㅂ", "씨ㅏㄹ", "ㅣ발", "ㅆ발", "ㅅ발", "ㅅㅂ", "ㅆㅂ", "ㅆ바", "ㅅ바", "시ㅂㅏ", "ㅅㅂㅏ", "시ㅏㄹ", "씨ㅏㄹ", "ㅅ불", "ㅆ불","ㅅ쁠", "ㅆ뿔", "씨8", "sipal","ㅅ ㅣ akwkksw바알","샤빨","쌰뺠","스벌","개존맛","TQ","tq","rotoRl","개좇돼","시방","tlqkf","w같은","신발련","뷰우웅신","좇","^^ㅣ벌","^^벌","씨벌","18아","tㅂ","siba","tq","qt","ㅆㅍ","Ribal","Libal"]
     for (const i of fuck) {
         if (text.includes(i)) {
             return true;
@@ -62,7 +62,7 @@ export function general(text: string|any): boolean {
     text = text.replace(/시바견/gi, '')
     text = text.replace(/시바/gi, '')
     text = text.replace(/이/gi, '')
-    fuck = ["시발", "씨발", "시봘", "씨봘", "씨바", "시바", "샤발", "씌발", "씹발", "시벌", "시팔", "싯팔", "씨빨", "씨랼", "씨파", "띠발", "띡발", "띸발","싸발", "십발", "슈발","야발", "씨불", "씨랄", "쉬발", "쓰발", "쓔발", "쌰발", "쉬발", "쒸발", "씨팔", "씨밝", "씨밯", "쑤발", "치발", "샤발", "발씨", "리발", "씨볼","찌발", "씨비바라랄", "시바랄", "씨바라"]
+    fuck = ["시발", "씨발", "시봘", "씨봘", "씨바", "시바", "샤발", "씌발", "씹발", "시벌", "시팔", "싯팔", "씨빨", "씨랼", "씨파", "띠발", "띡발", "띸발","싸발", "십발", "슈발","야발", "씨불", "씨랄", "쉬발", "쓰발", "쓔발", "쌰발", "쉬발", "쒸발", "씨팔", "씨밝", "씨밯", "쑤발", "치발", "샤발", "발씨", "리발", "씨볼","찌발", "씨비바라랄", "시바랄", "씨바라","슈벌","씹할","씹하다","십새","Sival","Sibal","시불","시부렝","시부랭"]
     for (const i of fuck) {
         if (text.includes(i)) {
             return true;
@@ -146,7 +146,7 @@ export function general(text: string|any): boolean {
     text = newtext.replace(/\^/gi, 'ㅅ')
     text = text.replace(/H/gi, 'ㅐ')
     text = text.replace(/[^가-힣]/gi, '')
-    const sonofbitch = ["ㅅㄲ", "ㅅ끼", "ㅆ끼", "쎆", "쌖","쌔꺄","새꺄"]
+    const sonofbitch = ["ㅅㄲ", "ㅅ끼", "ㅆ끼", "쎆", "쌖","쌔꺄","새꺄","씹색"]
     for (const i of sonofbitch) {
         if (text.includes(i)) {
             return true;
@@ -197,169 +197,9 @@ export function general(text: string|any): boolean {
         }
     }
 
-    return false;
-}
-
-
-function minor(text: string|any): boolean {
-    const newtext = text.toLowerCase()
     text = newtext.replace(/[^가-힣]/gi, '')
-        const sonofagun = ["개같", "개가튼", "개쉑", "개스키", "개세끼", "개색히", "개가뇬", "개새기", "개쌔기", "개쌔끼", "쌖", "쎆", "새긔", "개소리", "개년", "개소리",
-                    "개드립"]
+        const  sonofagun = ["개같", "개가튼", "개쉑", "개스키", "개세끼", "개색히", "개가뇬", "개새기", "개쌔기", "개쌔끼", "쌖", "쎆", "새긔", "개소리", "개년", "개소리","개드립","개샊","씹색","십새","Sib새"]
         for (const i of sonofagun) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-
-    text = newtext.replace(/[^ㄱ-힣]/gi, '')
-    const damnit = ["ㅁㅊ", "ㅁ친", "ㅁ쳤"]
-    for (const i of damnit) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-    text = newtext.replace(/[^가-힣]/gi, '')
-    text = text.replace(/이미친/gi, '')
-    text = text.replace(/미친증/gi, '')
-    const damnit2 = ["미친", "미쳤"]
-    for (const i of damnit2) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-
-    text = newtext.replace(/[^가-힣]/gi, '')
-    const picking = ["꼽냐", "꼽니", "꼽나"]
-    for (const i of picking) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function sexual(text: string|any): boolean {
-    const newtext = text.toLowerCase()
-    text = newtext.replace(/[^ㄱ-힣]/gi, '')
-    text = text.replace(/보지도못/gi, '')
-    text = text.replace(/보지도않/gi, '')
-    text = text.replace(/켜보지/gi, '')
-    text = text.replace(/보지맙/gi, '')
-    text = text.replace(/초보지/gi, '')
-    text = text.replace(/로보지/gi, '')
-    text = text.replace(/홍보지/gi, '')
-    text = text.replace(/서보지/gi, '')
-    text = text.replace(/보지금/gi, '')
-    text = text.replace(/정지금/gi, '')
-    text = text.replace(/걸보지/gi, '')
-    text = text.replace(/을보지/gi, '')
-    text = text.replace(/나보지/gi, '')
-    text = text.replace(/면접보지/gi, '')
-    text = text.replace(/보지는/gi, '')
-    text = text.replace(/보지지/gi, '')
-    text = text.replace(/못보지/gi, '')
-    text = text.replace(/보지않/gi, '')
-    text = text.replace(/보지마/gi, '')
-    text = text.replace(/보지말/gi, '')
-    text = text.replace(/안보지/gi, '')
-    text = text.replace(/오/gi, '')
-    const pussy = ["보지"]
-    for (const i of pussy) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-
-
-    text = newtext.replace(/[^ㄱ-힣]/gi, '')
-    const onahole = ["ㅈㅈ빨", "자ㅈ", "ㅈ지빨"]
-    for (const i of onahole) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-    text = newtext.replace(/[^ㄱ-힣]/gi, '')
-    text = text.replace(/남자지/gi, '')
-    text = text.replace(/여자지/gi, '')
-    text = text.replace(/감자지/gi, '')
-    text = text.replace(/왁자지/gi, '')
-    text = text.replace(/자지금/gi, '')
-    text = text.replace(/자지러/gi, '')
-    text = text.replace(/개발자지/gi, '')
-    text = text.replace(/타자지/gi, '')
-    text = text.replace(/못보지/gi, '')
-    text = text.replace(/자지원/gi, '')
-    text = text.replace(/사용자/gi, '')
-    text = text.replace(/자지않/gi, '')
-    text = text.replace(/자지마/gi, '')
-    text = text.replace(/자지말/gi, '')
-    text = text.replace(/지원지/gi, '')
-    text = text.replace(/안자지/gi, '')
-    text = text.replace(/아/gi, '')
-    const dicks = ["자지"]
-    for (const i of dicks) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-
-
-    text = newtext.replace(/\^/gi, 'ㅅ')
-    text = text.replace(/sex/gi, '섹스')
-    text = text.replace(/엑/gi, '')
-    text = text.replace(/[^ㄱ-힣]/gi, '')
-    const sex = ["ㅅㅔㄱ스", "섹ㅅ", "ㅅ스", "세ㄱㅅ", "ㅅㅔㄱㅅ"]
-    for (const i of sex) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-    text = newtext.replace(/[^가-힣]/gi, '')
-    text = text.replace(/야스오/gi, '')
-    const sex2 = ["섹스", "섻스", "쉑스", "섿스", "섹그", "야스", "색스", "셱스", "섁스", "세엑스", "썩스"]
-    for (const i of sex2) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-
-    text = newtext.replace(/[^가-힣]/gi, '')
-    const fireegg = ["불알", "부랄", "뽕알", "뿅알", "뿌랄", "뿔알", "블알"]
-    for (const i of fireegg) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-
-    text = newtext.replace(/[^ㄱ-힣]/gi, '')
-    const onahole2 = ["오나홍", "오나홀", "ㅇㄴ홀"]
-    for (const i of onahole2) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-    
-
-    text = newtext.replace(/[^가-힣]/gi, '')
-    const onahole3 = ["매춘부"]
-    for (const i of onahole3) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-
-    text = newtext.replace(/[^가-힣]/gi, '')
-    const onahole4 = ["자궁문신"]
-    for (const i of onahole) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-
-    text = newtext.replace(/[^가-힣]/gi, '')
-    const onahole5 = ["모유물","로리물"]
-    for (const i of onahole5) {
         if (text.includes(i)) {
             return true;
         }
