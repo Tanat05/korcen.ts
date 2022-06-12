0ed73554655776b79036848cf526a22f7ec3d008
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-06-12 21:33:24 +0900

    Update checkSpecial.ts

--- src/checkSpecial.ts
+++ src/checkSpecial.ts
@@ -2,9 +2,9 @@ export function special(text: string|any): boolean {
     if(!text) throw new Error('Korean: 확인할 텍스트를 입력해 주세요');
     if(typeof text !== 'string') throw new Error('Korean: String 타입만 입력 가능합니다');
     const newtext = text.toLowerCase()
-    const emoji = ["🖕🏻", "👌🏻👈🏻", "👉🏻👌🏻", "🤏🏻", "🖕", "🖕🏼", "🖕🏽", "🖕🏾", "🖕🏿"]
+    const emoji = ["🖕🏻", "👌🏻👈🏻", "👉🏻👌🏻", "🤏🏻", "🖕", "🖕🏼", "🖕🏽", "🖕🏾", "🖕🏿",":middle_finger:"]
     for (const i of emoji) {
-        if (newtext.includes(i)) {
+        if (text.includes(i)) {
             return true;
         }
     }
