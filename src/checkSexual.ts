43dba3660b43e927957f26c2745f78ae154ea9d6
Tanat <85154556+Tanat05@users.noreply.github.com>
2025-04-22 00:50:20 +0900

    Update checkSexual.ts

--- src/checkSexual.ts
+++ src/checkSexual.ts
@@ -1,212 +1,159 @@
-export function sexual(text: string | any): boolean {
-    if (!text) throw new Error('Korcen: 확인할 텍스트를 입력해 주세요');
-    if (typeof text !== 'string') throw new Error('Korean: String 타입만 입력 가능합니다');
-    const newtext = text.toLowerCase()
+const SINGLE_CHAR_NORMALIZATION_MAP = {
+    '𝗌': 's', '𝘴': 's', '𝙨': 's', '𝚜': 's', '𝐬': 's', '𝑠': 's', '𝒔': 's', '𝓈': 's', '𝓮': 's', '𝔰': 's', '𝖘': 's', '𝕤': 's',
+    'ｓ': 's', 'ş': 's', 'ⓢ': 's', '⒮': 's', '🅢': 's', '🆂': 's', '🅂': 's', '𝑺': 's', 'ſ': 's', 'š': 's', 'ś': 's', 'ŝ': 's',
+    'ṣ': 's', 'ṡ': 's', 'ș': 's', 'ṥ': 's', 'ṧ': 's', 'ṩ': 's', '$': 's', '5': 's',
 
-    const pussy = ["ⓑⓞⓩⓘ", "bozi", "보ㅈㅣ"]
-    for (const i of pussy) {
-        if (newtext.includes(i)) {
-            return true;
-        }
-    }
-    text = newtext.replace(/[^ㄱ-힣]/gi, '')
-    text = text.replace(/보g/gi, "보지")
-    text = text.replace(/보지도못/gi, '')
-    text = text.replace(/보지도않/gi, '')
-    text = text.replace(/켜보지/gi, '')
-    text = text.replace(/보지맙/gi, '')
-    text = text.replace(/초보지/gi, '')
-    text = text.replace(/로보지/gi, '')
-    text = text.replace(/홍보지/gi, '')
-    text = text.replace(/서보지/gi, '')
-    text = text.replace(/보지금/gi, '')
-    text = text.replace(/정지금/gi, '')
-    text = text.replace(/걸보지/gi, '')
-    text = text.replace(/을보지/gi, '')
-    text = text.replace(/나보지/gi, '')
-    text = text.replace(/면접보지/gi, '')
-    text = text.replace(/영화보지/gi, '')
-    text = text.replace(/애니보지/gi, '')
-    text = text.replace(/만화보지/gi, '')
-    text = text.replace(/사진보지/gi, '')
-    text = text.replace(/그림보지/gi, '')
-    text = text.replace(/보지는/gi, '')
-    text = text.replace(/보지지/gi, '')
-    text = text.replace(/못보지/gi, '')
-    text = text.replace(/보지않/gi, '')
-    text = text.replace(/보지마/gi, '')
-    text = text.replace(/보지말/gi, '')
-    text = text.replace(/안보지/gi, '')
-    text = text.replace(/보지도마/gi, '')
-    text = text.replace(/보지는않/gi, '')
-    text = text.replace(/정보/gi, '')
-    text = text.replace(/지팡이/gi, '')
-    text = text.replace(/행보/gi, '')
-    text = text.replace(/생각해보지/gi, '')
-    text = text.replace(/생각안해보지/gi, '')
-    text = text.replace(/뭘해보지/gi, '')
-    text = text.replace(/해보지/gi, '')
-    text = text.replace(/바보지/gi, '')
-    text = text.replace(/인가보지/gi, '')
-    text = text.replace(/오/gi, '')
-    const pussy1 = ["보지", "버지물", "버짓물", "보짓", "ⓑⓞⓩⓘ",
-        "bozi", "개보즤", "개보지", "버지벌렁벌렁", "보짖", "뵤즤", "봊이"]
-    for (const i of pussy1) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
+    '𝖾': 'e', '𝘦': 'e', '𝙚': 'e', '𝚎': 'e', '𝐞': 'e', '𝑒': 'e', '𝒆': 'e', 'ℯ': 'e', '𝓮': 'e', '𝔢': 'e', '𝖊': 'e', '𝕖': 'e',
+    'ｅ': 'e', 'ė': 'e', 'ⓔ': 'e', '⒠': 'e', '🅔': 'e', '🅴': 'e', '🄴': 'e', 'є': 'e', 'ê': 'e', 'ë': 'e', 'é': 'e', 'è': 'e',
+    'ē': 'e', 'ĕ': 'e', 'ě': 'e', 'ę': 'e', 'ẹ': 'e', 'ẻ': 'e', 'ẽ': 'e', 'ế': 'e', 'ề': 'e', 'ệ': 'e', 'ễ': 'e', 'ể': 'e',
+    '3': 'e', '€': 'e',
 
-    const dicks = ["ja지"]
-    for (const i of dicks) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-    text = newtext.replace(/[^ㄱ-힣]/gi, '')
-    const onahole1 = ["ㅈㅈ빨", "자ㅈ", "ㅈ지빨"]
-    for (const i of onahole1) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-    text = newtext.replace(/[^ㄱ-힣]/gi, '')
-    text = text.replace(/남자지/gi, '')
-    text = text.replace(/여자지/gi, '')
-    text = text.replace(/감자지/gi, '')
-    text = text.replace(/왁자지/gi, '')
-    text = text.replace(/자지금/gi, '')
-    text = text.replace(/자지러/gi, '')
-    text = text.replace(/개발자/gi, '')
-    text = text.replace(/관리자/gi, '')
-    text = text.replace(/타자지/gi, '')
-    text = text.replace(/혼자/gi, '')
-    text = text.replace(/못자지/gi, '')
-    text = text.replace(/자지원/gi, '')
-    text = text.replace(/사용자/gi, '')
-    text = text.replace(/자지않/gi, '')
-    text = text.replace(/자지마/gi, '')
-    text = text.replace(/자지말/gi, '')
-    text = text.replace(/지원자/gi, '')
-    text = text.replace(/안자지/gi, '')
-    text = text.replace(/부자지/gi, '')
-    text = text.replace(/자지도마/gi, '')
-    text = text.replace(/자지는않/gi, '')
-    text = text.replace(/혜자지/gi, '')
-    text = text.replace(/아/gi, '')
-    const dicks1 = ["자지", "자짓", "잦이"]
-    for (const i of dicks1) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
+    '𝗑': 'x', '𝘹': 'x', '𝙭': 'x', '𝚡': 'x', '𝐱': 'x', '𝑥': 'x', '𝒙': 'x', '𝓍': 'x', '𝔁': 'x', '𝔵': 'x', '𝖝': 'x', '𝕩': 'x',
+    'ｘ': 'x', 'ⓧ': 'x', '⒳': 'x', '🅧': 'x', '🆇': 'x', '🅇': 'x', '×': 'x', '✕': 'x', '✖': 'x', '❌': 'x', '⨯': 'x',
+    '⚔': 'x', '*': 'x', '✗': 'x', '✘': 'x',
 
-    text = newtext.replace(/cess/gi, '')
-    text = text.replace(/exit/gi, '')
-    const sex = ["sex", "s스", "x스", "se스", "se스", "s스", "ㅅㅅ", "s하고e싶다x", "ㅅㅔㅅㄱ"]
-    for (const i of sex) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-    text = newtext.replace(/\^/gi, 'ㅅ')
-    text = text.replace(/sex/gi, '섹스')
-    text = text.replace(/엑/gi, '')
-    text = text.replace(/[^ㄱ-힣]/gi, '')
-    const sex1 = ["ㅅㅔㄱ스", "섹ㅅ", "ㅅ스", "세ㄱㅅ", "ㅅㅔㄱㅅ"]
-    for (const i of sex1) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
-    text = newtext.replace(/[^가-힣]/gi, '')
-    text = text.replace(/야스오/gi, '')
-    text = text.replace(/크시야/gi, '')
-    text = text.replace(/카구야/gi, '')
-    text = text.replace(/스파이/gi, '')
-    text = text.replace(/말이야/gi, '')
-    text = text.replace(/스티브/gi, '')
-    text = text.replace(/스쿼드/gi, '')
-    text = text.replace(/스탑/gi, '')
-    text = text.replace(/스틸/gi, '')
-    text = text.replace(/스킬/gi, '')
-    text = text.replace(/스포티파이/gi, '')
-    text = text.replace(/스토리/gi, '')
-    text = text.replace(/스파게티/gi, '')
-    text = text.replace(/스릴러/gi, '')
-    text = text.replace(/스시/gi, '')
-    text = text.replace(/스레기/gi, '')
-    const sex2 = ["섹스", "섻", "쉑스", "섿스", "섹그", "야스", "색스", "셱스", "섁스", "세엑스", "썩스", "섹수", "섹파", "섹하자", "쉐스", "쉑스", "쉐엑스", "색수", "세엑수우", "섹하고",
-        "섹하구", "섹하자", "섹하장", "섹하쟈", "섹한번"]
-    for (const i of sex2) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
+    'ų': 'u', 'ü': 'u', 'ú': 'u', 'ù': 'u', 'û': 'u', 'ũ': 'u', 'ū': 'u', 'ŭ': 'u', 'ů': 'u', 'ű': 'u', 'ụ': 'u', 'ư': 'u',
+    'ç': 'c', 'ć': 'c', 'ĉ': 'c', 'č': 'c', 'ċ': 'c', '¢': 'c', '©': 'c', 'ḉ': 'c', '(': 'c', '<': 'c',
+    'Ｆ': 'F', 'Ḟ': 'F', 'Ƒ': 'F', 'ℱ': 'F', 'Ꞙ': 'F', 'Ꝼ': 'F',
+    'Ｋ': 'K', 'Ḱ': 'K', 'Ǩ': 'K', 'Ḳ': 'K', 'Ḵ': 'K', 'Ⱪ': 'K', 'Ꝁ': 'K',
+    'Ｃ': 'C', 'Ć': 'C', 'Ĉ': 'C', 'Č': 'C', 'Ċ': 'C', 'Ç': 'C', 'Ḉ': 'C',
+    'Ｕ': 'U', 'Ú': 'U', 'Ù': 'U', 'Û': 'U', 'Ũ': 'U', 'Ū': 'U', 'Ŭ': 'U', 'Ů': 'U', 'Ű': 'U', 'Ụ': 'U',
 
-    const dick3 = ["꼬3", "꼬툭튀", "꼬톡튀"]
-    for (const i of dick3) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
+    'ㅗ': 'ㅗ', '┻': 'ㅗ', '┴': 'ㅗ', '┹': 'ㅗ', '⊥': 'ㅗ', '†': 'ㅗ', '⟂': 'ㅗ', '╨': 'ㅗ', '╧': 'ㅗ', '╥': 'ㅗ',
+    '^': 'ㅅ', '人': 'ㅅ', '∧': 'ㅅ', '㉦': 'ㅅ', 'ᐲ': 'ㅅ', 'Λ': 'ㅅ', '⩘': 'ㅅ', '⋀': 'ㅅ', '⩚': 'ㅅ',
+    '甘': 'ㅂ', '廿': 'ㅂ', 'ᗨ': 'ㅂ', 'ᗐ': 'ㅂ', 'ᗕ': 'ㅂ', '田': 'ㅂ', '口': 'ㅂ', '日': 'ㅂ', '目': 'ㅂ', '囗': 'ㅂ',
+    '己': 'ㄹ', '乙': 'ㄹ', '已': 'ㄹ', '巳': 'ㄹ', '匚': 'ㄷ',
+    '卜': 'ㅏ', 'r': 'ㅏ', 'F': 'ㅏ', '丨': 'ㅏ', '|': 'ㅏ', 'ㅣ': 'ㅏ', '/': 'ㅏ', '⼃': 'ㅏ', '⼁': 'ㅏ', '⼂': 'ㅏ',
+    'l': 'ㅣ', '1': 'ㅣ', 'I': 'ㅣ', '!': 'ㅣ', '¦': 'ㅣ', '｜': 'ㅣ', '￤': 'ㅣ', 'І': 'ㅣ', 'Ӏ': 'ㅣ',
+    'H': 'ㅐ', 'ㅖ': 'ㅐ', 'ㅒ': 'ㅐ', 'Н': 'ㅐ', 'Ⲏ': 'ㅐ', 'ℍ': 'ㅐ',
+    '🐦': '새', '🐔': '새', '🦅': '새', '🦉': '새', '🦆': '새', '🦜': '새', '🦤': '새', '🦢': '새', '🕊': '새',
+    '🐕': '개', '🐶': '개', '🐺': '개',
+    '丕': '조', '朝': '조', '則': '조', '兆': '조', '組': '조', '早': '조', '鳥': '조', '潮': '조', '照': '조',
+    '0': 'ㅇ', 'O': 'ㅇ', 'o': 'ㅇ', '◯': 'ㅇ', '⭕': 'ㅇ', '○': 'ㅇ', '●': 'ㅇ', '◎': 'ㅇ', '◉': 'ㅇ', '◌': 'ㅇ',
 
-    text = newtext.replace(/[^가-힣]/gi, '')
-    const sex3 = ["씹하다"]
-    for (const i of sex3) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
+    'a': 'a', '@': 'a', '4': 'a', 'α': 'a', 'ä': 'a', 'å': 'a', 'ã': 'a', 'ā': 'a', 'ȧ': 'a', 'ǎ': 'a',
+    'b': 'b', '8': 'b', '6': 'b', 'ƃ': 'b', 'ɓ': 'b', 'Ƅ': 'b', 'ℬ': 'b', 'ᖯ': 'b', 'ᑲ': 'b',
+    'd': 'd', 'ḋ': 'd', 'ḍ': 'd', 'ᑯ': 'd', 'ᗞ': 'd', 'ᗪ': 'd', 'ᖙ': 'd', 'ⅆ': 'd', 'ɗ': 'd',
+    'f': 'f', 'ƒ': 'f', 'ḟ': 'f', 'ſ': 'f', 'ⅎ': 'f', 'ᶂ': 'f', 'ꜰ': 'f', 'ꟻ': 'f',
+    'g': 'g', '9': 'g', 'ǥ': 'g', 'ɡ': 'g', 'ġ': 'g', 'ģ': 'g', 'ĝ': 'g', 'ǧ': 'g',
+    'h': 'h', 'ĥ': 'h', 'ħ': 'h', 'ƕ': 'h', 'ḥ': 'h', 'ḫ': 'h', 'ⱨ': 'h', 'ꜧ': 'h',
+    'i': 'i', '1': 'i', '!': 'i', '|': 'i', 'ī': 'i', 'ĭ': 'i', 'ǐ': 'i', 'į': 'i',
+    'j': 'j', 'ĵ': 'j', 'ǰ': 'j', 'ȷ': 'j', 'ɉ': 'j', 'ⱼ': 'j', 'ʝ': 'j', 'ɟ': 'j',
+    'k': 'k', 'ķ': 'k', 'ƙ': 'k', 'ǩ': 'k', 'ḱ': 'k', 'ḳ': 'k', 'ḵ': 'k', 'ⱪ': 'k',
+    'l': 'l', '1': 'l', '|': 'l', 'ĺ': 'l', 'ļ': 'l', 'ľ': 'l', 'ŀ': 'l', 'ł': 'l',
+    'm': 'm', 'ɱ': 'm', 'ḿ': 'm', 'ṁ': 'm', 'ṃ': 'm', 'ⱥ': 'm', 'ᵯ': 'm', 'ᴍ': 'm',
+    'n': 'n', 'ń': 'n', 'ň': 'n', 'ñ': 'n', 'ņ': 'n', 'ṅ': 'n', 'ṇ': 'n', 'ṉ': 'n',
+    'p': 'p', 'ṕ': 'p', 'ṗ': 'p', 'ƥ': 'p', 'ᵽ': 'p', 'ᵱ': 'p', 'ᴘ': 'p', 'ᑭ': 'p',
+    'q': 'q', '9': 'q', 'ʠ': 'q', 'ɋ': 'q', 'ȹ': 'q', 'ⱊ': 'q', 'ⱍ': 'q', 'ꝗ': 'q',
+    'r': 'r', 'ŕ': 'r', 'ř': 'r', 'ŗ': 'r', 'ṙ': 'r', 'ṛ': 'r', 'ṝ': 'r', 'ṟ': 'r',
+    't': 't', '7': 't', '+': 't', 'ť': 't', 'ţ': 't', 'ŧ': 't', 'ț': 't', 'ṫ': 't',
+    'v': 'v', 'ṿ': 'v', 'ⱴ': 'v', 'ᵥ': 'v', 'ᵛ': 'v', '√': 'v', 'ᐱ': 'v', '∨': 'v',
+    'w': 'w', 'ẁ': 'w', 'ẃ': 'w', 'ẅ': 'w', 'ŵ': 'w', 'ẇ': 'w', 'ẉ': 'w', 'ⱳ': 'w',
+    'y': 'y', 'ý': 'y', 'ỳ': 'y', 'ŷ': 'y', 'ÿ': 'y', 'ȳ': 'y', 'ẏ': 'y', 'ỵ': 'y',
+    'z': 'z', '2': 'z', 'ź': 'z', 'ẑ': 'z', 'ž': 'z', 'ż': 'z', 'ẓ': 'z', 'ẕ': 'z',
+};
 
-    text = newtext.replace(/[^가-힣]/gi, '')
-    const fireegg = ["불알", "부랄", "뽕알", "뿅알", "뿌랄", "뿔알", "개부달", "개부랄", "개부러럴", "개부럴", "개부뢀", "개부알", "개불알", "똘추", "똥구멍"]
-    for (const i of fireegg) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
+const MULTI_CHAR_REPLACEMENTS = {
+    '_ㅣ_': 'ㅗ', '_/_': 'ㅗ', '_ |\_': 'ㅗ', '_|\_': 'ㅗ', '_ㅣ\_': 'ㅗ', '_I_': 'ㅗ',
+    '／＼': 'ㅅ', '/＼': 'ㅅ',
+    '77': 'ㄲ',
+    '刀卜': '까',
+    '₨': 'rs',
+    'ㅇl=스': '섹스',
+    'ㅇㅣ-ㅣ': '애',
+    'lㅣ': '니',
+    'ㅁㅣ': '미',
+    '丕刀卜己卜人丨廿卜己卜口卜': '조까씹쌔끼',
+};
 
-    text = newtext.replace(/[^ㄱ-힣]/gi, '')
-    const onahole2 = ["오나홍", "오나홀", "ㅇㄴ홀", "텐가", "바이브레이터", "오ㄴ홀", "ㅇ나홀"]
-    for (const i of onahole2) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
+const FALSE_POSITIVE_PATTERNS_SEXUAL = [
+    '보지도못', '보지도않', '인가보지', '면접보지', '영화보지', '애니보지', '만화보지', '사진보지',
+    '그림보지', '을보지', '나보지', '못보지', '안보지', '왜보지', '뭐보지', '다보지', '빨리보지',
+    '보지도마', '보지는않', '보지안으', '보지안아', '게보지', '어케보지', '하나보지', '켜보지',
+    '보지맙', '초보지', '로보지', '가보지', '홍보지', '서보지', '보지금', '보지못', '정지금',
+    '걸보지', '보지는', '보지지', '보지않', '해보지', '보지마', '보지말', '안보지만', '정보',
+    '지팡이', '행보', '바보지', '바보짓', '물어보지', '하시나보지',
+    '언제자지', '잠자지', '자지말자고', '지급', '남자지', '여자지', '감자지', '왁자지', '자지러',
+    '개발자', '관리자', '약탈자', '타자지', '혼자', '자지원', '사용자', '경력자', '지식', '자지마',
+    '자지말', '지원자', '부자지', '혜자지', '잘자지', '일자지', '일찍자지', '지원', '자지금', '자지않',
+    '어케자지', '자지도마', '자지는않', '자지좀마', '안자지', '못자지', '지건', '감자',
+    'cess', '```css', 'ex)', 'exit', 'ext', 'images', 'https', '(ex', '.ex', 'physics', 'features', 'exam',
+    'phase', 'except', 'sexual', 'sexy', '엑섹스', '엑',
+    '야스오', '크시야', '카구야', '스파이', '말이야', '스티브', '스쿼드', '파랑색', '오야스미', '노란색',
+    '빨간색', '초록색', '보라색', '청색', '핑크색', '남색', '검은색', '하양색', '주황색', '연두색',
+    '스공', '스시', '스키장', '스킨', '스킬', '스틸', '스탑', '스트레스', '해야', '카시야스', '야스톤', '유니섹스', '스튜디오',
+    '위대한', '소유자', '작업자', '자기위로', '위대하지', '암살자', '학자',
+    'freenude',
+];
+
+const SEXUAL_PROFANITY_PATTERNS = [
+    'ⓑⓞⓩⓘ', 'bozi', '보ㅈㅣ', '보지', '버지물', '버짓물', '보짓', '개보즤', '개보지', '버지벌렁벌렁', '보짖', '뵤즤', '봊이', '보g',
+    'ja지', 'ㅈㅈ빨', '자ㅈ', 'ㅈ지빨', '자지', '자짓', '잦이', '쟈지',
+    'sex', 's스', 'x스', 'se스', 'ㅅㅅ', 's하고e싶다x', 'ㅅㅔㅅㄱ', '이=스', '섹ㅅ', '세ㄱㅅ', '섹스', '섻', '쉑스', '섿스', '섹그', '야스', '색스', '셱스', '섁스', '세엑스', '썩스', '섹수', '섹파', '섹하자', '쉐스', '쉐엑스', '색수', '세엑수우', '섹하고', '섹하구', '섹하장', '섹하쟈', '섹한번', '쌕스',
+    '꼬3', '꼬툭튀', '꼬톡튀', '불알', '부랄', '뽕알', '뿅알', '뿌랄', '뿔알', '개부달', '개부랄', '개부러럴', '개부럴', '개부뢀', '개부알', '개불알', '똘추', '똥구멍', '부라랄',
+    '오나홍', '오나홀', 'ㅇㄴ홀', '텐가', '바이브레이터', '오ㄴ홀', 'ㅇ나홀', '씹하다', '매춘부', '성노예', '자궁문신',
+    '모유물', '로리물', '근친상간', '룸섹스', '원조교재', '속박플레이', '야외플레이',
+    '딸딸이', '질싸', '안에사정', '자위남', '자위녀', '폰섹', '포르노', '폰세엑', '폰쉑', '폰쎅', '질내사정', '그룹섹', '남창', '男色', '누워라이년아', '누웠냐씨방새', '다리벌려', '대줄년', '뒤로너어줘', '딸따뤼', '딸쳐', '떡쳐라', '막대쑤셔줘', '막대핥아줘', '먹고보니내딸', '먹고보니누나', '먹고보니딸', '먹고보니똥개', '먹고보니엄마', '먹고보니응아', '먹고보니재수', '먹고보니처제', '먹고보니형수', '몸뚱이줄께', '몸안에사정', '밖에다쌀께', '박고빼고', '배위에싸죠', '몸의대화', '섹할', '섹해',
+    'g스팟', '지스팟', '크리토리스', '클리토리스', '페니스', '애널', '젖까', '젖가튼', '젖나', '젖만',
+    'ja위', '자위', '고자새끼', '고츄', '꺼추', '꼬추',
+];
+
+const allFalsePositivePatterns = [
+    ...FALSE_POSITIVE_PATTERNS_SEXUAL
+];
+allFalsePositivePatterns.sort((a, b) => b.length - a.length);
+const ALL_FP_REGEX = new RegExp(allFalsePositivePatterns.map(escapeRegex).join('|'), 'gi');
 
 
-    text = newtext.replace(/[^가-힣]/gi, '')
-    const onahole3 = ["매춘부", "성노예"]
-    for (const i of onahole3) {
-        if (text.includes(i)) {
-            return true;
-        }
+const sortedSEXUALProfanityPatterns = [...SEXUAL_PROFANITY_PATTERNS];
+sortedSEXUALProfanityPatterns.sort((a, b) => b.length - a.length);
+
+
+function escapeRegex(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+const singleCharMapRegex = new RegExp(Object.keys(SINGLE_CHAR_NORMALIZATION_MAP).map(key => escapeRegex(key)).join('|'), 'gi');
+
+const multiCharReplacementRegex = new RegExp(
+    Object.keys(MULTI_CHAR_REPLACEMENTS)
+    .map(escapeRegex)
+    .sort((a, b) => b.length - a.length)
+    .join('|'),
+    'gi'
+);
+
+
+export function check(text: string | any): boolean {
+    if (typeof text !== 'string') {
+        if (!text) throw new Error('korcen: 확인할 텍스트를 입력해 주세요');
+        throw new Error('korcen: String 타입만 입력 가능합니다');
+    }
+    if (!text.trim()) {
+        throw new Error('korcen: 확인할 텍스트를 입력해 주세요');
     }
 
-    text = newtext.replace(/[^가-힣]/gi, '')
-    const onahole4 = ["자궁문신"]
-    for (const i of onahole4) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
+    let processedText = text.toLowerCase().replace(/ /gi, '');
 
-    text = newtext.replace(/[^가-힣]/gi, '')
-    const onahole5 = ["모유물", "로리물", "근친상간", "룸섹스", "원조교재", "속박플레이", "야플", "야외플레이"]
-    for (const i of onahole5) {
-        if (text.includes(i)) {
-            return true;
-        }
-    }
+    processedText = processedText.replace(singleCharMapRegex, match => {
+        const lowerMatch = match.toLowerCase();
+        return SINGLE_CHAR_NORMALIZATION_MAP[lowerMatch] || match;
+    });
 
-    text = newtext.replace(/[^가-힣]/gi, '')
-    const belittling = ["딸딸이", "질싸", "안에사정", "자위남", "자위녀", "폰섹", "포르노", "폰세엑", "폰쉑", "폰쎅", "질내사정", "그룹섹", "남창", "男色", "누워라이년아",
-        "누웠냐씨방새", "다리벌려", "대주까", "대줄년", "뒤로너어줘", "딸따뤼", "딸쳐", "떡쳐라", "막대쑤셔줘", "막대핥아줘", "먹고보니내딸", "먹고보니누나", "먹고보니딸",
-        "먹고보니똥개", "먹고보니엄마", "먹고보니응아", "먹고보니재수", "먹고보니처제", "먹고보니형수", "몸뚱이줄께", "몸안에사정", "밖에다쌀께", "박고빼고",
-        "배위에싸죠", "g스팟", "지스팟", "크리토리스", "클리토리스", "페니스", "애널", "젖까", "젖가튼", "젖나", "젖만", "ja위", "자위", "고자새끼", "고츄", "꺼추", "께세", "꼬추"]
-    for (const i of belittling) {
-        if (text.includes(i)) {
+    processedText = processedText.replace(multiCharReplacementRegex, match => {
+        const lowerMatch = match.toLowerCase();
+        for (const key in MULTI_CHAR_REPLACEMENTS) {
+            if (key.toLowerCase() === lowerMatch) {
+                return MULTI_CHAR_REPLACEMENTS[key];
+            }
+        }
+        return match;
+    });
+
+    processedText = processedText.replace(ALL_FP_REGEX, '');
+
+    for (const pattern of sortedSEXUALProfanityPatterns) {
+        if (processedText.includes(pattern.toLowerCase())) {
             return true;
         }
     }
